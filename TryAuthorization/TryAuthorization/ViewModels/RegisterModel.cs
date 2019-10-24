f0661b5a8bea37e7f1767ddb8ba73718ce967897
Victoria Dzyuba <47743417+mohorka@users.noreply.github.com>
2019-10-24 13:57:25 +0300

    Add simple authorization
    Model of simple login and registration

---
+++ TryAuthorization/TryAuthorization/ViewModels/RegisterModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace TryAuthorization.ViewModels
+{
+    public class RegisterModel
+    {
+       
+        
+            [Required(ErrorMessage = "Не указан email")]
+            public string Email { get; set; }
+            [Required(ErrorMessage = "Не указан пароль")]
+            [DataType(DataType.Password)]
+            public string Password { get; set; }
+            [DataType(DataType.Password)]
+            [Compare("Password",ErrorMessage ="Введен неверный пароль")]
+            public string ConfirmPassword { get; set; }
+        
+
+    }
+}
